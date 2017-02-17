39dc5096a84a30523ff46890965a7497667bd6ef
Morgan Nomura <morganelle@gmail.com>
2017-02-17 10:57:27 -0800

    updated some todo answers

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,15 +1,15 @@
 'use strict';
 
-// TODO: Do not forget to go into your SQL shell and DROP TABLE the existing articles/authors tables. Be sure to start clean.
+// DONE: Do not forget to go into your SQL shell and DROP TABLE the existing articles/authors tables. Be sure to start clean.
 const pg = require('pg');
 const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
-// TODO: Don't forget to set your own conString if required by your system
+// DONE: Don't forget to set your own conString if required by your system
 const conString = 'postgres://localhost:5432';
-// TODO: Using a sentence or two, describe what is happening in Line 12.
-// Put your response here...
+// DONE: Using a sentence or two, describe what is happening in Line 12.
+// Sets a constant of 'client' to a new instance of pg.Client that takes the conString path as an argument.  client.connect on line 14 will begin communication with the postgres server.
 const client = new pg.Client(conString);
 client.connect();
 
@@ -28,11 +28,10 @@ app.get('/new', function(request, response) {
 
 // Following are the routes for making API calls to enact CRUD Operations on our database
 
-// TODO: Some of the following questions will refer back to the image called 'full-stack-diagram' that has been added to the lab directory. In that image you will see that the various parts of the application's activity have been numbered 1-5. When prompted in the following questions, identify which number best matches the location of a given process. For instance, the following line of code, where the server is handling a request from the view layer, would match up with #2.
+// DONE: Some of the following questions will refer back to the image called 'full-stack-diagram' that has been added to the lab directory. In that image you will see that the various parts of the application's activity have been numbered 1-5. When prompted in the following questions, identify which number best matches the location of a given process. For instance, the following line of code, where the server is handling a request from the view layer, would match up with #2.
 app.get('/articles', function(request, response) {
   // REVIEW: We now have two queries which create separate tables in our DB, and reference the authors in our articles.
-  // TODO: What number in the full-stack diagram best matches what is happening in lines 35-42?
-  // Put your response here...
+  // TODO: What number in the full-stack diagram best matches what is happening in lines 35-42? #3
   client.query(`
     CREATE TABLE IF NOT EXISTS
     authors (
@@ -51,12 +50,12 @@ app.get('/articles', function(request, response) {
       "publishedOn" DATE,
       body TEXT NOT NULL
     );`
-  ) // TODO: Referring to lines 45-52, answer the following questions:
+  ) // DONE: Referring to lines 45-52, answer the following questions:
     // What is a primary key?
-    // Put your response here...
+    // It is the unique identifier for a record in a table
     // +++++++++++++++++++++
     // What does VARCHAR mean?
-    // Put your response here...
+    // It specifies a field populated by a string of variable length; the (n) after VARCHAR defines a max length.
     // +++++++++++++++++++++
   // REVIEW: This query will join the data together from our tables and send it back to the client.
   client.query(`
@@ -70,8 +69,8 @@ app.get('/articles', function(request, response) {
   );
 });
 
-// TODO: How is a 'post' route different than a 'get' route?
-// Put your answer here...
+// DONE: How is a 'post' route different than a 'get' route?
+// A 'post' route inserts, while a 'get' route reads or selects
 app.post('/articles', function(request, response) {
   client.query(
     'INSERT INTO authors(author, "authorUrl") VALUES($1, $2) ON CONFLICT DO NOTHING', // DONE: Write a SQL query to insert a new author, ON CONFLICT DO NOTHING
@@ -84,8 +83,8 @@ app.post('/articles', function(request, response) {
 
   function queryTwo() {
     client.query(
-      // TODO: What is the purpose of the $1 in the following line of code?
-      // Put your response here...
+      // DONE: What is the purpose of the $1 in the following line of code?
+      // The $1 acts as a variable which is populated by information from the HTTP request in the array below.
       `SELECT author_id FROM authors WHERE author=$1`, // DONE: Write a SQL query to retrieve the author_id from the authors table for the new article
       [request.body.author], // DONE: Add the author name as data for the SQL query
       function(err, result) {
@@ -96,7 +95,8 @@ app.post('/articles', function(request, response) {
   }
 
   function queryThree(author_id) {
-      // TODO: What number in the full-stack diagram best matches what is happening in line 100?
+      // DONE: What number in the full-stack diagram best matches what is happening in line 100?
+      //#3
     client.query(
       `INSERT INTO
       articles(author_id, title, category, "publishedOn", body)
@@ -110,7 +110,7 @@ app.post('/articles', function(request, response) {
       ], // DONE: Add the data from our new article, including the author_id, as data for the SQL query.
       function(err) {
         if (err) console.error(err);
-        // TODO: What number in the full-stack diagram best matches what is happening in line 114?
+        // DONE: What number in the full-stack diagram best matches what is happening in line 114? #5
         response.send('insert complete');
       }
     );
@@ -130,7 +130,7 @@ app.put('/articles/:id', function(request, response) {
 
   function queryTwo(author_id) {
     client.query(
-      // TODO: In a sentence or two, describe how a SQL 'UPDATE' is different from an 'INSERT', and identify which REST verbs and which CRUD components align with them.
+      // DONE: In a sentence or two, describe how a SQL 'UPDATE' is different from an 'INSERT', and identify which REST verbs and which CRUD components align with them. SQL UPDATE permits a query that changes the value of a field in a specific record. It corresponds with CRUD UPDATE. SQL INSERT adds a new record to a table, and corresponds with CRUD CREATE.
       `UPDATE authors
       SET author=$1, "authorUrl"=$2
       WHERE author_id=$3;`, // DONE: Write a SQL query to update an existing author record
@@ -159,16 +159,16 @@ app.put('/articles/:id', function(request, response) {
   }
 });
 
-  // TODO: What number in the full-stack diagram best matches what is happening in line 163?
+  // DONE: What number in the full-stack diagram best matches what is happening in line 163? #2
 app.delete('/articles/:id', function(request, response) {
-    // TODO: What number in the full-stack diagram best matches what is happening in lines 165?
+    // DONE: What number in the full-stack diagram best matches what is happening in lines 165? #3
   client.query(
     `DELETE FROM articles WHERE article_id=$1;`,
-    // TODO: What does the value in 'request.params.id' come from? If unsure, look in the Express docs.
-    // Put your response here...
+    // DONE: What does the value in 'request.params.id' come from? If unsure, look in the Express docs.
+    // 'request' is from the callback function's parameters and represents the HTTP request in this statement
     [request.params.id]
   );
-  // TODO: What number in the full-stack diagram best matches what is happening in line 171?
+  // DONE: What number in the full-stack diagram best matches what is happening in line 171? #5
   response.send('Delete complete');
 });
 
@@ -183,4 +183,4 @@ app.listen(PORT, function() {
   console.log(`Server started on port ${PORT}!`);
 });
 
-// TODO: Make your own drawing of the full-stack diagram on a blank piece of paper (there is a stack of paper on the table next to the door into our classroom) and submit to the TA who grades your lab assignments. This is for just a little extra reinforcement of how everything works.
+// DONE per ERICA: Make your own drawing of the full-stack diagram on a blank piece of paper (there is a stack of paper on the table next to the door into our classroom) and submit to the TA who grades your lab assignments. This is for just a little extra reinforcement of how everything works.
